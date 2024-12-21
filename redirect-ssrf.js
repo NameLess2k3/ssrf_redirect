361870249da2bd9218328e28697e52df4eb76e84
thai chi bach <bachtheman25@gmail.com>
2024-12-20 21:40:52 -0800

    version 2

--- redirect-ssrf.js
+++ redirect-ssrf.js
@@ -4,7 +4,13 @@ const app = express();
 let reqCounter = 0;
 
 app.get('/', (req, res) => {
+    if (reqCounter === 0) {
+        reqCounter += 1;
+        res.send('an innocent site uwu');
+    } else {
+        reqCounter -= 1;
         res.redirect('http://localhost:1337/flag');
+    }
 });
 
 const PORT = 4444;
